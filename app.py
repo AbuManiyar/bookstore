b2ce3d952057afd90bccc71203f291560cb96273
Abutalha <maniyarabutalha00@gmail.com>
2023-07-27 09:36:56 +0530

    'db,html'

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mysql.connector as conn
 db = conn.connect(host = 'localhost', user= 'root', password = '')
 cursor = db.cursor()
@@ -6,18 +6,37 @@ cursor = db.cursor()
 app = Flask(__name__)
 
 
+class Book:
+    def __init__(self, name, price, stock):
+        self.name= name
+        self.price= price
+        self.stock= stock
+        cursor.execute("create database if not exists books")
+        cursor.execute("use abutalha")
+        cursor.execute("create table if not exists book(name varchar(20), price int, stock int)")
+        cursor.execute(f"insert into book value('{self.name}', {self.price}, {self.stock})")
+        
+        
+    def update(self,presentstock):
+        self.stock = presentstock
+        
 @app.route('/')
 def home():
-    cursor.execute("create database if not exists books")
-    return "This is a page to create a Book Store"
+   return render_template("home.html")
 
-@app.route('/add')
+@app.route('/add', methods=["POST"])
 def addbook():
-    cursor.execute("use abutalha")
-    cursor.execute("select * from bank_details")
+    name = request.form["Name"]
+    price = int(request.form["Price"])
+    stock = int(request.form["Stock"])
+    book = Book(name,price,stock)
+    
     #print(cursor.fetchall())
-    return cursor.fetchall()
+    return "Book Added"
 
-
+@app.route('/bookadd')
+def bookadd():
+    return render_template("addbook.html") 
+    
 if __name__ == "__main__":
     app.run(debug = True)

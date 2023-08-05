adb9b54acda00fc008f5d4d021bdf3d5a5ce8d06
Abutalha <maniyarabutalha00@gmail.com>
2023-08-05 08:09:52 +0530

    update sell and delete

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector as conn
-db = conn.connect(host = 'localhost', user= 'root', password = '@123')
+db = conn.connect(host = 'localhost', user= 'root', password = 'Abua@123')
 cursor = db.cursor()
 app = Flask(__name__)
 
@@ -28,10 +28,11 @@ def addbook():
     name = request.form["Name"]
     price = int(request.form["Price"])
     stock = int(request.form["Stock"])
+    name = name.strip()
     book = Book(name,price,stock)
-    
+    msg = "Book added"
     #print(cursor.fetchall())
-    return "<h1>Book Added</h1>"
+    return render_template("addbook.html", msg=msg)
 
 @app.route('/bookadd')
 def bookadd():
@@ -39,9 +40,44 @@ def bookadd():
 
 @app.route('/info')
 def info():
+    cursor.execute("create database if not exists books")
+    cursor.execute("use books")
+    cursor.execute("create table if not exists book1(name varchar(200), price int, stock int)")
     cursor.execute("select * from book1")
     book = cursor.fetchall()
-    return render_template('info.html', returnbooks = book )
+    numbooks= len(book)
+    cursor.execute("select sum(stock) from book1")
+    numberofbooks = cursor.fetchall()
+    return render_template('info.html', returnbooks = book,numbooks =numbooks, numberofbooks=numberofbooks)
+    
+    
+@app.route("/delete/<i>")
+def delbook(i):
+    #n =  cursor.execute(f"select stock from book1 where name='{i}'")
+    cursor.execute(f"delete from book1 where name='{i}'")
+    #print(type(n))
+    #return f"<h1>deleted book {i}</h1>"
+    return redirect(url_for('info'))
+
+@app.route('/sold/<i>')
+def sold(i):
+    cursor.execute(f"delete from book1 where name='{i}'")
+    return redirect(url_for('info'))
+    
+@app.route('/sell/<i>/<s>')
+def sellbook(i,s):
+    ns=int(s)-1
+    if ns == 0:
+        cursor.execute(f"delete from book1 where name='{i}'")
+        return redirect(url_for('info'))
+    else:
+        cursor.execute(f"update book1 set stock={ns} where name='{i}'")
+        #return f'sold book {i}'    
+        return redirect(url_for('info'))
+    
+@app.route('/add')
+def firstbook():
+     return render_template('addbook.html')
     
 if __name__ == "__main__":
     app.run()
